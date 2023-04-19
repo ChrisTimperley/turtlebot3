26ba34ab0ef58a2c19f768ed59157fae709a377a
ChrisTimperley <christimperley@googlemail.com>
2023-04-19 14:57:57 -0400

    added gui launch argument to navigation2

--- turtlebot3_navigation2/launch/navigation2.launch.py
+++ turtlebot3_navigation2/launch/navigation2.launch.py
@@ -18,8 +18,8 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.actions import IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
@@ -81,5 +81,7 @@ def generate_launch_description():
             name='rviz2',
             arguments=['-d', rviz_config_dir],
             parameters=[{'use_sim_time': use_sim_time}],
-            output='screen'),
+            condition=IfCondition(LaunchConfiguration('gui', default='true')),
+            output='screen',
+        ),
     ])
